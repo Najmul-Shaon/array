7b5f8714285cc389a6ec6ea63104f7b4ac918a41
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-09-07 14:09:59 +0600

    looping.js

---
+++ looping.js
@@ -0,0 +1,12 @@
+const friends = ["Alice", "Bob", "Charlie", "David", "Emma"];
+for (const friend of friends) {
+//   console.log(friend);
+}
+for (let i = 0; i < friends.length; i++) {
+//   console.log(friends[i]);
+}
+let i = 0;
+while (i < friends.length) {
+  console.log(friends[i]);
+  i++;
+}
